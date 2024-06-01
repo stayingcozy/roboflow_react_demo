525ba41c2b6769d4ca899c2b1c795b15a694e81c
stayingcozy <cody.colclough@gmail.com>
2024-06-01 13:10:41 -0500

    logo and pics

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <main className="max-w-2xl mx-auto">
       Roboflow 🦝 + React ⚛️
       <div className="w-full h-full">
-        <Roboflow modelName="dog-emotions" modelVersion="1" />
+        <Roboflow modelName="dog-emotions" modelVersion="3" />
       </div>
     </main>
   )
